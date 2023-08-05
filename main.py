10144f21645180e2bfd73b99e57f9881c142f679
Atlas <46817089+AzeAstro@users.noreply.github.com>
2023-08-06 02:41:47 +0400

    Code upload

---
+++ main.py
@@ -0,0 +1,149 @@
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keycode import Keycode
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS as KeyboardLayout
+from board import *
+import digitalio
+import socketpool
+import usb_hid
+import wifi
+import time
+
+duckyCommands = {
+    'WINDOWS': Keycode.WINDOWS,'WIN': Keycode.WINDOWS, 'GUI': Keycode.GUI,
+    'APP': Keycode.APPLICATION, 'MENU': Keycode.APPLICATION, 'SHIFT': Keycode.SHIFT,
+    'ALT': Keycode.ALT, 'CONTROL': Keycode.CONTROL, 'CTRL': Keycode.CONTROL,
+    'DOWNARROW': Keycode.DOWN_ARROW, 'DOWN': Keycode.DOWN_ARROW, 'LEFTARROW': Keycode.LEFT_ARROW,
+    'LEFT': Keycode.LEFT_ARROW, 'RIGHTARROW': Keycode.RIGHT_ARROW, 'RIGHT': Keycode.RIGHT_ARROW,
+    'UPARROW': Keycode.UP_ARROW, 'UP': Keycode.UP_ARROW, 'BREAK': Keycode.PAUSE,
+    'PAUSE': Keycode.PAUSE, 'CAPSLOCK': Keycode.CAPS_LOCK, 'DELETE': Keycode.DELETE,
+    'END': Keycode.END, 'ESC': Keycode.ESCAPE, 'ESCAPE': Keycode.ESCAPE, 'HOME': Keycode.HOME,
+    'INSERT': Keycode.INSERT, 'NUMLOCK': Keycode.KEYPAD_NUMLOCK, 'PAGEUP': Keycode.PAGE_UP,
+    'PAGEDOWN': Keycode.PAGE_DOWN, 'PRINTSCREEN': Keycode.PRINT_SCREEN, 'ENTER': Keycode.ENTER,
+    'SCROLLLOCK': Keycode.SCROLL_LOCK, 'SPACE': Keycode.SPACE, 'TAB': Keycode.TAB,
+    'A': Keycode.A, 'B': Keycode.B, 'C': Keycode.C, 'D': Keycode.D, 'E': Keycode.E,
+    'F': Keycode.F, 'G': Keycode.G, 'H': Keycode.H, 'I': Keycode.I, 'J': Keycode.J,
+    'K': Keycode.K, 'L': Keycode.L, 'M': Keycode.M, 'N': Keycode.N, 'O': Keycode.O,
+    'P': Keycode.P, 'Q': Keycode.Q, 'R': Keycode.R, 'S': Keycode.S, 'T': Keycode.T,
+    'U': Keycode.U, 'V': Keycode.V, 'W': Keycode.W, 'X': Keycode.X, 'Y': Keycode.Y,
+    'Z': Keycode.Z, 'F1': Keycode.F1, 'F2': Keycode.F2, 'F3': Keycode.F3,
+    'F4': Keycode.F4, 'F5': Keycode.F5, 'F6': Keycode.F6, 'F7': Keycode.F7,
+    'F8': Keycode.F8, 'F9': Keycode.F9, 'F10': Keycode.F10, 'F11': Keycode.F11,
+    'F12': Keycode.F12,
+}
+
+
+led = digitalio.DigitalInOut(LED)
+led.direction = digitalio.Direction.OUTPUT
+
+kbd = Keyboard(usb_hid.devices)
+layout = KeyboardLayout(kbd)
+time.sleep(.5)
+
+defaultDelay=0
+previousLine = ""
+
+
+pool=socketpool.SocketPool(wifi.radio)
+
+
+def blink_led(led):
+    if led.value:
+        led.value = False
+    else:
+        led.value = True
+
+def convertLine(line):
+    newline = []
+    for key in filter(None, line.split(" ")):
+        key = key.upper()
+        command_keycode = duckyCommands.get(key, None)
+        if command_keycode is not None:
+            newline.append(command_keycode)
+        elif hasattr(Keycode, key):
+            newline.append(getattr(Keycode, key))
+        else:
+            print(f"Unknown key: <{key}>")
+    return newline
+
+def runScriptLine(line):
+    for k in line:
+        kbd.press(k)
+    kbd.release_all()
+
+def sendString(line):
+    layout.write(line)
+
+def parseLine(line):
+    global defaultDelay
+    if(line[0:3] == "REM"):
+        pass
+    elif(line[0:5] == "DELAY"):
+        time.sleep(float(line[6:])/1000)
+    elif(line[0:6] == "STRING"):
+        sendString(line[7:])
+    elif(line[0:13] == "DEFAULT_DELAY"):
+        defaultDelay = int(line[14:]) * 10
+    elif(line[0:12] == "DEFAULTDELAY"):
+        defaultDelay = int(line[13:]) * 10
+    else:
+        newScriptLine = convertLine(line)
+        runScriptLine(newScriptLine)
+
+
+def runPayload(duckyScript):
+    for line in duckyScript.splitlines():
+        line = line.rstrip()
+        if(line[0:6] == "REPEAT"):
+            for i in range(int(line[7:])):
+                parseLine(previousLine)
+                time.sleep(float(defaultDelay)/1000)
+        else:
+            parseLine(line)
+            previousLine = line
+        time.sleep(float(defaultDelay)/1000)
+
+
+
+
+
+
+def main():
+    while not wifi.radio.ipv4_address:
+        try:
+            wifi.radio.connect("Hekim","19751975")
+            blink_led(led)
+        except ConnectionError as e:
+            print("Connection Error:", e)
+            print("Retrying in 10 seconds")
+        time.sleep(10)
+
+
+
+    mySoc=pool.socket(pool.AF_INET,pool.SOCK_STREAM)
+    mySoc.bind(("0.0.0.0",9999))
+    mySoc.listen(1)
+
+
+    while True:
+        print("Waiting for connection...")
+        with mySoc.accept()[0] as connSoc:
+            print("Got connection.")
+            while True:
+                arrayData=bytearray(1024)
+                length=connSoc.recv_into(arrayData,1024)
+                data=bytes(arrayData[:length])
+                data=data.decode()
+                if data=="!disconnect":
+                    connSoc.close()
+                    break
+                elif data=="!stop":
+                    connSoc.close()
+                    exit()
+                else:
+                    runPayload(data)
+                    
+if __name__=="__main__":
+    main()
+
+
+
