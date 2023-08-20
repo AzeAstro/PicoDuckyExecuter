bc43bae5e9a856cff5e830dcdc89ce0c43837929
Atlas <46817089+AzeAstro@users.noreply.github.com>
2023-08-20 18:57:59 +0400

    Added comments
    Added comments for other readers and removed useless blink_led function. Plays no role in script.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.mouse import Mouse
 from adafruit_hid.keycode import Keycode
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS as KeyboardLayout
 from board import *
@@ -10,7 +11,8 @@ import wifi
 import time
 from struct import pack,unpack
 
-
+# Keys for DuckyScript
+# It will need us
 duckyCommands = {
     'WINDOWS': Keycode.WINDOWS,'WIN': Keycode.WINDOWS, 'GUI': Keycode.GUI,
     'APP': Keycode.APPLICATION, 'MENU': Keycode.APPLICATION, 'SHIFT': Keycode.SHIFT,
@@ -34,28 +36,30 @@ duckyCommands = {
     'F12': Keycode.F12,
 }
 
-
+# Defining led for wireless connectivity status
 led = digitalio.DigitalInOut(LED)
 led.direction = digitalio.Direction.OUTPUT
 led.value = False
 
+# Keyboard initialization
 kbd = Keyboard(usb_hid.devices)
 layout = KeyboardLayout(kbd)
+
+# Mouse initialization
+m = Mouse(usb_hid.devices)
+
+# Some sleep before hard work ;)
 time.sleep(.5)
 
+# Setting global variables such as defaultDelay and previousLine(The last line that got executed)
 defaultDelay=0
 previousLine = ""
 
-
+# Creation of socketPool object for working with sockets
 pool=socketpool.SocketPool(wifi.radio)
 
 
-def blink_led(led):
-    if led.value:
-        led.value = False
-    else:
-        led.value = True
-
+# convertLine command for converting buttons such as ALT, F4, ENTER and etc and adding it into button list
 def convertLine(line):
     newline = []
     for key in filter(None, line.split(" ")):
@@ -69,14 +73,17 @@ def convertLine(line):
             print(f"Unknown key: <{key}>")
     return newline
 
+# Run buttons in list that got returned by convertLine function
 def runScriptLine(line):
     for k in line:
         kbd.press(k)
     kbd.release_all()
 
+# Typing string that has been given as argument
 def sendString(line):
     layout.write(line)
 
+# This function is kinda handler. Handles the given line and calls functions according to given DuckyScript line
 def parseLine(line):
     global defaultDelay
     if(line[0:3] == "REM"):
@@ -94,6 +101,7 @@ def parseLine(line):
         runScriptLine(newScriptLine)
 
 
+# This function can be called as handler too but this function handles the whole payload.
 def runPayload(duckyScript):
     for line in duckyScript.splitlines():
         line = line.rstrip()
@@ -111,57 +119,91 @@ def runPayload(duckyScript):
 
 
 
-
+# Here the magic begins...
 def main():
+    # Here, we try to connect network. It is in endless loop, so, it won't stop unless it connects to wifi
     while not wifi.radio.ipv4_address:
         try:
             wifi.radio.connect("someWifi","somePass")
-            blink_led(led)
         except ConnectionError as e:
             print("Connection Error:", e)
             print("Retrying in 10 seconds")
         time.sleep(10)
+    
+    # Turns led on when got connected to wifi
+    led.value=True
 
-
-
+    # Creation of socket object for connection between client and Pico
     mySoc=pool.socket(pool.AF_INET,pool.SOCK_STREAM)
+    
+    # I chose port 9999 but you can modify it as you want.
     mySoc.bind(("0.0.0.0",9999))
+    
+    #Yep, listening to one client at time is prefered. Reason is scripts that comes from couple clients sometimes can get mixed.
+    # You don't want to someone else remove your shell backdoor script and type "start https://www.youtube.com/watch?v=dQw4w9WgXcQ" and then press enter, do you? ;))
     mySoc.listen(1)
 
-
+    # It is in while True loop because, we don't want to Pico shut down when someone disconnects.
+    # Pico will stop only when "!exit" command is received. And it will happen by raising error.
     while True:
         print("Waiting for connection...")
         with mySoc.accept()[0] as connSoc:
             print("Got connection.")
             
+            # Here, payload receiving starts...
             while True:
+                # Since, we don't want to limit payload length to some number, we want to receive its length before we receive the payload itself.
+                # Which means, we can send payload as huge has memory can handle. Not that 1024 or 2048 length ones.
+                # I used long int to send payload length. Long's limit is huge enough for one payload.
                 payloadLength=bytearray(4)
                 connSoc.recv_into(payloadLength,4)
                 payloadLength=unpack("l",payloadLength)[0]
+                
+                #Here, payload is getting received. 
                 payloadData=b""
-                print(payloadLength)
+                # Generic while True: socket.recv() loop but here, in CircuitPython, we got recv_into, not recv.
                 while True:
                     if len(payloadData)>=payloadLength:
                         break
                     else:
+                        # If payload length is over 2048, we receive it as chunks.
                         chunk=bytearray(2048)
                         connSoc.recv_into(chunk,2048)
                         payloadData+=chunk
+                        # Now, you might wonder why we use replace.
+                        # For the reason that I have no idea, when we sometimes receive payload, it gave some array of "\x00" in chunk.
+                        # Even when payload didn't containt it or it wasn't sent by client.
+                        # And before you say "Maybe last chunk contained it?", no it happened in full chunks as well. For example, payload length is 10K and it still had that null byte array at chunks that is in middle.
                         payloadData=payloadData.replace(b"\x00",b"")
+                
+                #Here, we convert received payload into string.
                 payloadData=payloadData.decode()
+                # It is a mini handler too.
+                # Handles if the command that it sent by client is disconnect/exit request or a DuckyScript payload
                 if payloadData=="!disconnect":
+                    # As I said earlier, it disconnects one client and waits for other one to connect.
                     connSoc.close()
                     break
                 elif payloadData=="!stop":
+                    # Here, exit() function raises error. Since CircuitPython doesn't have exit function like regular python, it can be used here.
+                    # Even if exit function existed, both variants(function and raised error) would do the same job for us.
                     connSoc.close()
                     exit()
                 else:
+                    # And here the payload is passed as argument into handler.
                     runPayload(payloadData)
+                
+                # GC is short for Garbage Collector. Here I use it clearing memory. It is really useful. Don't recommend to remove this line.
                 gc.collect()
                 
                     
 if __name__=="__main__":
+    # I made it this way so if you just need one function from this script, you can use it without running the code itself.
     main()
+
+# Now, if you read the whole comments written here, then now you know the magic of "Willy Wonka's chocholate factory" ;))
+# If you liked it, don't forget one fact: You can buy me one chocholate too ;)
+# Good luck with your script! 
 
 
 
